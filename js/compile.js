38b5cc41d46caf145291135f3daa258cd275c420
fchen <51083793+fchen-group@users.noreply.github.com>
2020-11-11 15:41:52 +0800

    Update compile.js

--- js/compile.js
+++ js/compile.js
@@ -1,13 +1,14 @@
-// 导入solc和fs
+// import solc fs
 let solc = require('solc')
 let fs = require('fs')
 
-// 读取合约代码
+// read contract
 let contractCode = fs.readFileSync('../contract/Service.sol', 'utf-8')
 
-// 编译合约代码
+// compile contract
 let output = solc.compile(contractCode, 1)
-// 执行导出
+
+// export 
 module.exports = output['contracts'][':Service']
 console.log('bytecode : ', output['contracts'][':Service']['bytecode'])
 console.log('abi : ', output['contracts'][':Service']['interface'])
